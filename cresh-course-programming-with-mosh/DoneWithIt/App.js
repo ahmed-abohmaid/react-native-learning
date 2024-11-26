44457d9aee7d2e5abddd6be5fb063e37f0916390
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-11-26 13:13:11 +0200

    refactor: format the file

--- cresh-course-programming-with-mosh/DoneWithIt/App.js
+++ cresh-course-programming-with-mosh/DoneWithIt/App.js
@@ -1,4 +1,12 @@
-import { StyleSheet, Text, SafeAreaView, Image, TouchableNativeFeedback, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableNativeFeedback,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const handleTextPress = () => console.log("Hello");
@@ -7,31 +15,35 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text onPress={handleTextPress}>Hello World!</Text>
       {/* works only on Android */}
-      <TouchableNativeFeedback> 
-        <View style={{
-          backgroundColor: "dodgerblue",
-          padding: 10,
-          borderRadius: 5,
-          marginTop: 10,
-        }}>
-          <Text style={{ color: "white", }}>Press me!</Text>
+      <TouchableNativeFeedback>
+        <View
+          style={{
+            backgroundColor: "dodgerblue",
+            padding: 10,
+            borderRadius: 5,
+            marginTop: 10,
+          }}
+        >
+          <Text style={{ color: "white" }}>Press me!</Text>
         </View>
       </TouchableNativeFeedback>
       <TouchableOpacity>
-        <Image source={{
-          uri: "https://picsum.photos/200/300",
-          width: 200,
-          height: 300
-        }} />
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300,
+          }}
+        />
       </TouchableOpacity>
-    </SafeAreaView >
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
